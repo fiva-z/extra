b60e6dc6a4f76a3e638df9251a5bda6c2cb0aa3c
fiva <105108559+fiva-z@users.noreply.github.com>
2026-06-11 21:48:36 +0200

    Update status.lua

--- Floader/TB/status.lua
+++ Floader/TB/status.lua
@@ -1,1 +1,1 @@
-return Status = "Undetected", StatusColor = Color3.fromRGB(0, 255, 0), LastUpdate = "11.6.26",
+return Status = "Undetected", StatusColor = Color3.fromRGB(0, 255, 0), LastUpdate = "11.6.26"
